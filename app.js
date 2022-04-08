b58aa56e77bd2394d9bda513d17dd1a0adc69777
Enrique Castañeda <mrstinky64@gmail.com>
2022-04-07 21:08:47 -0600

    problem with temp directory fixed

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
-var referenciaRoutes = require('./routes/referenciaRoute')
+var referenciaRoutes = require('./routes/referenciaRoute');
+const fs = require("fs");
 
 const PORT = process.env.PORT;
 
 app.use(bodyparser.json());
 app.use('/api', referenciaRoutes);
 
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
